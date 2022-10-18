3667d8b7928355b1a1d5b3fd6a5bea259b09d9bf
Richard Medina <medinarichard996@gmail.com>
2022-10-17 19:30:56 -0700

    getting started

--- README.md
+++
@@ -1,1 +0,0 @@
-# Wild-Fire-Project
---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello</div>
+    </div>
+  );
+}
+
+export default App;
