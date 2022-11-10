c1f7af4128a081505b28a093d5224a81e71b50ac
Richard Medina <medinarichard996@gmail.com>
2022-11-10 02:06:03 -0800

    Add landing page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import LandingPage from './landingPage.jsx'
 import map from './mapus.jpeg'
 function App() {
   return (
     <div className="App">
-        <img id='map' src={map}></img>
+      <LandingPage/>
+        {/* <img id='map' src={map}></img>
       <div id='predict'>
         <label for='prediction'>Select A Date For Prediction</label>
         <input type='date' id='prediction'></input>
-      </div>
+      </div> */}
     </div>
   );
 }
---
+++ front-end/src/landingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './landingPage.css'
+const LandingPage = () => {
+  return(<div id='background'>
+    <div id='title-container'>
+      <h1>Location-based <br></br> wildfire prediction</h1>
+    </div>
+    <button id='start-btn'>Start Here</button>
+  </div>)
+}
+
+export default LandingPage
