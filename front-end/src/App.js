0f8912ee55cbd05af0e4301678f028141a7a6bbb
a-zhongg <amyzhong16@gmail.com>
2022-12-04 22:44:24 -0800

    modified:   front-end/src/App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,6 +16,12 @@ function App() {
         </Routes>
       </Router>
 
+      <div id='predict'>
+        <label for='prediction'>Select A Date For Prediction</label>
+        <input type='date' id='prediction'></input>
+        <input type = "button" id="predictbutton" value ="Predict"></input>
+      </div>
+    
 
     </div>
   );
